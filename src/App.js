73a232faa4209b100f558444996d6324226a7285
nishant Gupta <ngactive99@gmail.com>
2021-06-11 14:44:22 +0530

    now using React-bootstrap module to create a better form

--- src/App.js
+++ src/App.js
@@ -1,58 +1,64 @@
 import './App.css';
 import React, { useState } from 'react';
-
+import { Button, Form } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
-  const [user, setUser] = useState({ firstName: "ram" , age:34});
+  const [user, setUser] = useState({ firstName: "ram", age: 34, gender: "Male" });
   const [users, setUsers] = useState([]);
-  console.log('rendering again');
   const handleEvent = e => {
-    console.log(e.target.value);
-    setUser(...user,{ [e.target.name]: e.target.value })
+    setUser( {...user, [e.target.name]: e.target.value });
+  }
+  const saveUser = () => {
+    const promise = fetch('http://localhost:4200/users', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    });
+    promise.then((response) => {
+      console.log(response);
+    })
   }
   return (
-    <div className="App">
-      <input value={user.firstName} name='firstName' onChange={handleEvent}></input>
-      <input value={user.age} name='age' onChange={handleEvent}></input>
+    <Form>
+      <Form.Group controlId="formBasicEmail">
+        <label>Normal label</label>
+        <Form.Label >Email address</Form.Label>
+        <Form.Control name='firstName' onChange={handleEvent} value={user.firstName} placeholder="Enter firstName" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+      </Form.Text>
+      </Form.Group>
 
-      <button onClick={() => {
-        console.log('saving');
-        const promise = fetch('http://localhost:4200/users', {
-          method: 'POST',
-          headers: {
-            'content-type': 'application/json'
-          },
-          body: JSON.stringify(user)
-
-        });
-        promise.then((response) => {
-          console.log(response);
-        })
-
-      }}>save</button>
-
-      <button onClick={() => {
-        console.log('gettingUsers');
-        const promise = fetch('http://localhost:4200/users')
-        promise.then((response) => {
-          response.json().then((body) => {
-            console.log(body);
-            setUsers(body);
-          })
-
-
-        })
-      }}>
-
-        get-users</button>
+      <Form.Group controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control onChange={handleEvent} value={user.age} type="number" placeholder="Age" name="age"/>
+      </Form.Group>
+      <Form.Group controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      {['radio'].map((type) => (
+        <div key={`inline-${type}`} className="mb-3">
+          <Form.Check onChange={handleEvent} value='Male' inline label="Male" name="gender"
+            type={type} id={`inline-${type}-1`} />
+          <Form.Check onChange={handleEvent} inline value='Female' label="Female" name="gender"
+            type={type} id={`inline-${type}-2`} />
+          <Form.Check  inline disabled
+            label="3(disabled)"
+            type={type}
+            id={`inline-${type}-3`}
+          />
+        </div>
+      ))}
+      <Button onClick={saveUser} variant="primary" type="button">
+        Submit
+    </Button>
+    </Form>
 
 
-      <table><thead>
-      </thead><th>Name</th>
-        <tbody>{users.map((user) => {
-          return <tr><td>{user.firstName}</td></tr>
-        })} </tbody></table>
-    </div>
   );
 }
+
 
 export default App;
