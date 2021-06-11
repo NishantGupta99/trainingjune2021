73b700100ed93521741d95790b1fdbc182881d9d
nishant Gupta <ngactive99@gmail.com>
2021-06-11 15:29:44 +0530

    added checkbox and its working in app.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,29 @@ import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
-  const [user, setUser] = useState({ firstName: "ram", age: 34, gender: "Male" });
+  const [user, setUser] = useState({ firstName: "ram", age: 34, gender: "Male", skills: [] });
   const [users, setUsers] = useState([]);
   const handleEvent = e => {
-    setUser( {...user, [e.target.name]: e.target.value });
+    if (e.target.type == 'checkbox') {
+      const target = e.target;
+      if (e.target.checked) {
+        //add values here
+        user[target.name].push(target.value);
+      } else {
+        //when user deselects       
+        let i = -1;
+        user[target.name].map((value, index) => {
+          if (value == target.value) {
+            i = index;
+          }
+        });
+        if (i > -1) {
+          user[target.name].splice(i, 1);
+        }
+      }
+      setUser({...user, [e.target.name]:user.skills});
+    } else
+      setUser({ ...user, [e.target.name]: e.target.value });
   }
   const saveUser = () => {
     const promise = fetch('http://localhost:4200/users', {
@@ -30,21 +49,30 @@ function App() {
           We'll never share your email with anyone else.
       </Form.Text>
       </Form.Group>
-
       <Form.Group controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control onChange={handleEvent} value={user.age} type="number" placeholder="Age" name="age"/>
+        <Form.Control onChange={handleEvent} value={user.age} type="number" placeholder="Age" name="age" />
       </Form.Group>
-      <Form.Group controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
+      {['checkbox'].map((type) => (
+        <div key={`inline-${type}`} className="mb-3">
+          <Form.Check onChange={handleEvent} value='Java'  inline label="Java" name="skills"
+            type={type} id={`inline-${type}-1`} />
+          <Form.Check onChange={handleEvent} inline value='Javascript' label="Javascript" name="skills"
+            type={type} id={`inline-${type}-2`} />
+          <Form.Check inline disabled
+            label="3(disabled)"
+            type={type}
+            id={`inline-${type}-3`}
+          />
+        </div>
+      ))}
       {['radio'].map((type) => (
         <div key={`inline-${type}`} className="mb-3">
           <Form.Check onChange={handleEvent} value='Male' inline label="Male" name="gender"
             type={type} id={`inline-${type}-1`} />
           <Form.Check onChange={handleEvent} inline value='Female' label="Female" name="gender"
             type={type} id={`inline-${type}-2`} />
-          <Form.Check  inline disabled
+          <Form.Check inline disabled
             label="3(disabled)"
             type={type}
             id={`inline-${type}-3`}
@@ -55,8 +83,6 @@ function App() {
         Submit
     </Button>
     </Form>
-
-
   );
 }
 
