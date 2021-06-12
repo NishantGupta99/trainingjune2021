06b9d1ae279f2fda61e5fa9ce40458c01bce46df
nishant Gupta <ngactive99@gmail.com>
2021-06-12 16:41:07 +0530

    Did the basic code for form validations

--- src/App.js
+++ src/App.js
@@ -3,9 +3,18 @@ import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
-  const [user, setUser] = useState({ firstName: "ram", age: 34, gender: "Male", skills: [] });
+  const [user, setUser] = useState({ firstName: "", age: 34, gender: "Male", skills: [] });
   const [users, setUsers] = useState([]);
+  const [ errors, setErrors ] = useState([]);
   const handleEvent = e => {
+    e.preventDefault()
+    
+    // Check and see if errors exist, and remove them from the error object:
+    if ( !!errors[e.target.name] ) setErrors({
+      ...errors,
+      [e.target.name]: null
+    })
+    
     if (e.target.type == 'checkbox') {
       const target = e.target;
       if (e.target.checked) {
@@ -23,11 +32,17 @@ function App() {
           user[target.name].splice(i, 1);
         }
       }
-      setUser({...user, [e.target.name]:user.skills});
+      setUser({ ...user, [e.target.name]: user.skills });
     } else
       setUser({ ...user, [e.target.name]: e.target.value });
   }
   const saveUser = () => {
+    const newErrors = findFormErrors();
+    if ( Object.keys(newErrors).length > 0 ) {
+      // We got errors!
+      setErrors(newErrors)
+      return
+    }
     const promise = fetch('http://localhost:4200/users', {
       method: 'POST',
       headers: {
@@ -39,15 +54,35 @@ function App() {
       console.log(response);
     })
   }
+  /*const setField = (field, value) => {
+    setUser({
+      ...user,
+      [field]: value
+    })
+    // Check and see if errors exist, and remove them from the error object:
+    if ( !!errors[field] ) setErrors({
+      ...errors,
+      [field]: null
+    })
+  }*/
+  
+
+  const findFormErrors = () => {
+    const newErrors ={}
+    if( !user.firstName || user.firstName=== '') newErrors.firstName = 'this field cannot be blank'
+    else if ( user.firstName.length > 30) newErrors.firstName = "the name is too big"
+    return newErrors
+  }
   return (
     <Form>
       <Form.Group controlId="formBasicEmail">
         <label>Normal label</label>
         <Form.Label >Email address</Form.Label>
-        <Form.Control name='firstName' onChange={handleEvent} value={user.firstName} placeholder="Enter firstName" />
+        <Form.Control name='firstName' onChange={handleEvent} value={user.firstName} placeholder="Enter firstName" required />
         <Form.Text className="text-muted">
           We'll never share your email with anyone else.
-      </Form.Text>
+        </Form.Text>
+        <Form.Control.Feedback type='invalid'> {errors.firstName} </Form.Control.Feedback>
       </Form.Group>
       <Form.Group controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
@@ -55,15 +90,11 @@ function App() {
       </Form.Group>
       {['checkbox'].map((type) => (
         <div key={`inline-${type}`} className="mb-3">
-          <Form.Check onChange={handleEvent} value='Java'  inline label="Java" name="skills"
+          <Form.Check onChange={handleEvent} value='Java' inline label="Java" name="skills"
             type={type} id={`inline-${type}-1`} />
           <Form.Check onChange={handleEvent} inline value='Javascript' label="Javascript" name="skills"
             type={type} id={`inline-${type}-2`} />
-          <Form.Check inline disabled
-            label="3(disabled)"
-            type={type}
-            id={`inline-${type}-3`}
-          />
+
         </div>
       ))}
       {['radio'].map((type) => (
@@ -72,16 +103,12 @@ function App() {
             type={type} id={`inline-${type}-1`} />
           <Form.Check onChange={handleEvent} inline value='Female' label="Female" name="gender"
             type={type} id={`inline-${type}-2`} />
-          <Form.Check inline disabled
-            label="3(disabled)"
-            type={type}
-            id={`inline-${type}-3`}
-          />
         </div>
       ))}
-      <Button onClick={saveUser} variant="primary" type="button">
+      <Button onClick={saveUser} variant="primary" type="button" >
+        
         Submit
-    </Button>
+      </Button>
     </Form>
   );
 }
