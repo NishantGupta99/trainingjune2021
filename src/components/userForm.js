ad4cce1e360ea09f725f15d677e3d56cf357bef3
nishant Gupta <ngactive99@gmail.com>
2021-06-15 21:41:11 +0530

    added axios and made a list of all the names available inside database

--- src/components/userForm.js
+++ src/components/userForm.js
@@ -1,10 +1,32 @@
-   
-import React, { useState } from 'react';
-import { Button, Form } from 'react-bootstrap';
 
- const UserForm = () => {
+import React, { useState } from 'react';
+import { Button, Form,ListGroup } from 'react-bootstrap';
+import axios from 'axios';
+
+const UserForm = () => {
     const [user, setUser] = useState({ firstName: "ram", age: 34, gender: "Male", skills: [] });
     const [users, setUsers] = useState([]);
+
+
+    function getAllUsers() {
+        axios.get('http://localhost:4200/users')
+            .then(function (response) {
+                // handle success
+                console.log(response.data);
+               setUsers(response.data)
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .then(function () {
+                // always executed
+            });
+
+
+    }
+   
+    getAllUsers();
     const handleEvent = e => {
         if (e.target.type === 'checkbox') {
             const target = e.target;
@@ -73,16 +95,19 @@ import { Button, Form } from 'react-bootstrap';
                             type={type} id={`inline-${type}-1`} />
                         <Form.Check onChange={handleEvent} inline value='Female' label="Female" name="gender"
                             type={type} id={`inline-${type}-2`} />
-                        <Form.Check inline disabled
-                            label="3(disabled)"
-                            type={type}
-                            id={`inline-${type}-3`}
-                        />
+
                     </div>
                 ))}
                 <Button onClick={saveUser} variant="primary" type="button">
                     Submit
                 </Button>
+                <ListGroup>
+                  { users.map((user,index)=>{
+                       return  <ListGroup.Item>{user.firstName}</ListGroup.Item>
+                   })}
+                  
+                </ListGroup>
+
             </Form>
 
         </div>
